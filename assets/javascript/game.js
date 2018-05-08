bef5d891ebb8a194a7c1e781f327178e672629a0
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-05-08 08:09:26 -0400

    HTML and JS updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// declare variables
+
+// initialize game (use on.document.ready)
+
+    // initialize game function: use a for loop to iterate through the "gems" and assign a random number to each
+
+// CONDITIONAL LOGIC
+
