67e05f20e6209a439a59401154faaec479f574aa
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-05-11 22:59:50 -0400

    adding completed game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,63 @@
-// declare variables
+var scoreTarget = 0;
+var scoreTally = 0;
 
-// initialize game (use on.document.ready)
+var numberOfGems = 4;
+var assignedGemValue = [];
+var gems = ["assets/images/gem-0.png", "assets/images/gem-1.png", "assets/images/gem-2.png", "assets/images/gem-3.png"];
 
-    // initialize game function: use a for loop to iterate through the "gems" and assign a random number to each
+var wins = 0;
+var losses = 0;
 
-// CONDITIONAL LOGIC
+var main = $("body");
 
+$(document).ready(function () {
+
+    resetGame();
+
+    $(".points").click(function () {
+        var currentGemValue = ($(this).attr("scoreTally-value"));
+        console.log("clicked " + currentGemValue);
+
+        currentGemValue = parseInt(currentGemValue);
+        scoreTally += currentGemValue;
+        console.log("current score: " + scoreTally);
+        $("#progressToTarget").text("Your total score is: " + scoreTally);
+        // W/L logic
+        if (scoreTally === scoreTarget) {
+            wins++;
+            $("#gameWins").text("Wins: " + wins);
+            resetGame();
+        } else if (scoreTally >= scoreTarget) {
+            losses++;
+            $("#gameLosses").text("Losses: " + losses);
+            resetGame();
+        }
+    });
+
+});
+
+function resetGame() {
+    // regenerate targetNumber
+    scoreTarget = Math.floor((Math.random() * 120) + 19)
+    $("#targetNumber").text("Target: " + scoreTarget);
+    console.log("scoreTarget: " + scoreTarget);
+    // reset scoreTally
+    scoreTally = 0;
+    $("#progressToTarget").text("Your total score is: " + scoreTally);
+    console.log("scoreTally: " + scoreTally);
+    // reset gem images
+    $("#gem-images").empty();
+    var img = main.find("#gem-images");
+
+    for (i = 0; i < gems.length; i++) {
+        // create the point values for the gem images
+        assignedGemValue[i] = Math.floor((Math.random() * 12) + 1);
+
+        var gemImg = $("<img>");
+        gemImg.addClass("col-3 points");
+        gemImg.attr("src", gems[i]);
+        gemImg.attr("scoreTally-value",assignedGemValue[i]);
+        img.append(gemImg);
+    }
+    console.log("assignedGemValue: " + assignedGemValue)
+}
